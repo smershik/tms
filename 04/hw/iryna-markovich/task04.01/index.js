52d1f7e856b970682ba02dc6a2be7c67d2ad83ac
Iryna Markovich <irina.markovich13@gmail.com>
2018-06-13 17:42:26 +0300

    fixed names

--- 04/hw/iryna-markovich/task04.01/index.js
+++ 04/hw/iryna-markovich/task04.01/index.js
@@ -51,21 +51,18 @@ let task2 = new Task("Task");
 let task3 = new Task("Task Task Task");
 let task4 = new Task("Task Task Task Task");
 
-let arrTask = [];
-arrTask[0] = task1;
-arrTask[1] = task2;
-arrTask[2] = task3;
-arrTask[3] = task4;
+let tasks = [];
+tasks.push(task1, task2, task3, task4);
 
 let container = document.createElement('div');
 container.className = "container";
 document.body.appendChild(container);
 
 
-for (let i = 0; i < arrTask.length; i++) {
+for (let i = 0; i < tasks.length; i++) {
   let taskBox = document.createElement('div');
   taskBox.className = "task-box";
-  taskBox.innerHTML = arrTask[i].id + ' ' + arrTask[i].text + "<br>" + "<b>" + arrTask[i].dateFormatCreate + "</b>";
+  taskBox.innerHTML = tasks[i].id + ' ' + tasks[i].text + "<br>" + "<b>" + tasks[i].dateFormatCreate + "</b>";
   container.appendChild(taskBox);
 }
 
