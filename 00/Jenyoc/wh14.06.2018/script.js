32c7066c9c5927dc7e80aeb9397cba950c7ba305
Jenyoc <Jenyoc@mail.ru>
2018-06-15 03:13:21 +0300

    script.js

--- 00/Jenyoc/wh14.06.2018/script.js
+++ 00/Jenyoc/wh14.06.2018/script.js
@@ -1,4 +1,5 @@
-function Task(text) {
+window.onload = function() {
+  function Task(text) {
   this.text=text;
   var timeNow=new Date();
   Object.defineProperty(this, 'timeNow',{
@@ -6,18 +7,20 @@ function Task(text) {
       return timeNow;
     }
   })
-}
-
-(function() {
-  var out=document.getElementsByClassName('.output');
-  var inp=document.getElementsByClassName('.input1');
-  document.querySelector('.input1').addEventListener('keydown', function(enter) {
-    if (enter.keyCode === 13) {
-      var task=new Task(inp.value);
-      var div = document.createElement('div');
-      div.className = "alert alert-success";
-      div.innerHTML = task.text+'===>>>>' +task.timeNow;
-      document.body.appendChild(div);
-    }
+  }
+  (function() {
+    var inp=document.getElementsByClassName('.input1');
+    document.querySelector('.input1').addEventListener('keydown', function(enter) {
+      if (enter.keyCode === 13) {
+        var task=new Task(inp.value);
+        var div = document.createElement('div');
+        div.className = "newBlock";
+        div.innerHTML = task.text/*Выводит undifined*/+'===>>>>' +task.timeNow;
+        document.body.appendChild(div);
+      }
+    });
+  })();
+  document.querySelector('.del').addEventListener('onclick', function () {
+    div.parentNode.removeChild(div);/*Это вообще не работает*/
   });
-})();
+};
