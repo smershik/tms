bc6e195343bfb739b34f77ecc2fe6d86cc219071
Jenyoc <Jenyoc@mail.ru>
2018-06-29 19:10:29 +0300

    Домашка

--- 00/Jenyoc/script.js
+++ 00/Jenyoc/script.js
@@ -1,57 +1,64 @@
 (function () {
-  var newsId, getSources, gn, block,
+  var newsId, sources, block,
     url = 'https://newsapi.org/v2/sources?apiKey=3e04d75c3df0462c91e62abf7c794fb2&language=ru',
 
     input=  document.querySelector('.inp'),
     newsBlock= document.querySelector('.newsBlock'),
-    xhrContainer = document.querySelector('.news'),
+    container = document.querySelector('.news'),
     hide=document.querySelector('.hide'),
 
     xhr= new XMLHttpRequest();
+
   xhr.open('GET',url);
   xhr.send();
 
   xhr.onreadystatechange=function(){
-    console.log(xhr.readyState);
     if(xhr.readyState!==4) return;
-    getSources=JSON.parse(xhr.response).sources;
-    renderXhr(xhr);
+    sources=JSON.parse(xhr.response).sources;
+    render();
   };
-  /*ща будет пздц костыль...*/
+
   function hideNews(event){
     newsBlock.innerText='';
   }
+
   function getGn(event) {
     hideNews();
     newsId='google-news-ru';
-    giveMeTheNews();
+    getNews();
   }
+
   function getLenta(event) {
     hideNews();
     newsId='lenta';
-    giveMeTheNews();
+    getNews();
   }
+
   function getRbc(event) {
     hideNews();
     newsId='rbc';
-    giveMeTheNews();
+    getNews();
   }
+
   function getRt(event) {
     hideNews();
     newsId='rt';
-    giveMeTheNews();
+    getNews();
   }
-  function giveMeTheNews() {
-    var urlNews = 'https://newsapi.org/v2/top-headlines?apiKey=3e04d75c3df0462c91e62abf7c794fb2&sources='+newsId;
-    var news=new XMLHttpRequest();
-    news.open('GET',urlNews);
-    news.send();
-    news.onreadystatechange=function(){
-      console.log(news.readyState);
-      if(news.readyState!==4) return;
-      getNews=JSON.parse(news.response).articles;
-      console.log(getNews);
-      getNews.forEach(function (item) {
+
+  function getNews() {
+    var urlNews = 'https://newsapi.org/v2/top-headlines?apiKey=3e04d75c3df0462c91e62abf7c794fb2&sources='+newsId,
+      newsXhr=new XMLHttpRequest();
+
+    newsXhr.open('GET',urlNews);
+    newsXhr.send();
+
+    newsXhr.onreadystatechange=function(){
+      if(newsXhr.readyState!==4) return;
+      news=JSON.parse(newsXhr.response).articles;
+      console.log(news);
+
+      news.forEach(function (item) {
         block=createElement('section', {
         });
         block.innerHTML='<a href="' + item.url + '">'+
@@ -62,6 +69,7 @@
       });
     };
   }
+
   function createElement(elemName, options, attributes){
     attributes = attributes || {};
     var elem = document.createElement(elemName);
@@ -71,14 +79,14 @@
     });
     return elem;
   }
-  function renderXhr(xhr){
-    var gnID = createElement('button', {innerText: getSources[0].id, className:'gn'}),
-      lentaID = createElement('button', {innerText: getSources[1].id, className:'lenta'}),
-      rbcID = createElement('button', {innerText: getSources[2].id, className:'rbc'}),
-      rtID = createElement('button', {innerText: getSources[3].id, className:'rt'}),
-      xhrBox = createElement('DIV');
-    xhrBox.innerHTML += getElemsHtml([gnID,lentaID,rbcID,rtID]);
-    xhrContainer.appendChild(xhrBox);
+  function render(){
+    var gnID = createElement('button', {innerText: sources[0].id, className:'gn'}),
+      lentaID = createElement('button', {innerText: sources[1].id, className:'lenta'}),
+      rbcID = createElement('button', {innerText: sources[2].id, className:'rbc'}),
+      rtID = createElement('button', {innerText: sources[3].id, className:'rt'}),
+      box = createElement('DIV');
+    box.innerHTML = getElemsHtml([gnID,lentaID,rbcID,rtID]);
+    container.appendChild(box);
     var gn=document.querySelector('.gn'),
       lenta=document.querySelector('.lenta'),
       rbc=document.querySelector('.rbc'),
